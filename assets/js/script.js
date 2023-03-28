dc27e3f1c1e43fef3219effad5b4ce788e7538ef
Laura Mendez <lauramendez@Lauras-Air.lan>
2023-03-27 18:35:48 -0700

    final output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-//    if (tasks.length){
-//      $('.description').text(tasks);
-//     }
-//     $('.saveBtn').on('click', function(){
-//        var time = $(this).parent().attr('id');
-//        var userInput = $(this).siblings('.description').val();
-//        tasks.push(userInput);
-//        localStorage.setItem(time,JSON.stringify(userInput));
-//        window.location.reload();
-//  });
+//
  
-
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -48,7 +36,7 @@ $(function () {
     //console.log(this.id);
     var blockId = parseInt(this.id.split('-')[1]);
     //console.log(blockId);
-    var $el= $(this)
+    var $el = $(this)
     if (blockId < currentTime) {
       $el.addClass('past');
     } else if (blockId === currentTime) {
@@ -76,7 +64,7 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page. -->still need to add number suffixes st,nd,th rd Do 
   var today = dayjs();
   $('#currentDay').text(today.format('dddd,MMMM D YYYY'));
-  
+   
 });
 
 
